755ce26db68ad3f4ec90985a71a480b734754e77
k-paulius <k.dash.paulius@gmail.com>
2023-04-27 04:28:28 +0000

    Refactor DB library; implement create activity /w hardcoded user handle firsty_lasty_02

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -195,7 +195,7 @@ def data_search():
 @app.route("/api/activities", methods=['POST','OPTIONS'])
 @cross_origin()
 def data_activities():
-  user_handle  = 'andrewbrown'
+  user_handle = 'firsty_lasty_02'
   message = request.json['message']
   ttl = request.json['ttl']
   model = CreateActivity.run(message, user_handle, ttl)

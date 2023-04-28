e60a3c4c2a8038018028a090adbd1f2d485addd5
k-paulius <k.dash.paulius@gmail.com>
2023-04-28 04:25:19 +0000

    Implement DynamoDB integration

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,13 +1,14 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, handle, email, cognito_user_id)
 VALUES
-  ('Andrew Brown', 'andrewbrown', 'andrew_brown@cruddur.com', 'MOCK'),
-  ('Andrew Bayko', 'bayko', 'andrew_bayko@cruddur.com', 'MOCK');
+  ('Firsty Lasty The First',  'firsty_lasty_01', 'firsty_lasty_01@cruddur.com', 'MOCK'),
+  ('Firsty Lasty The Second', 'firsty_lasty_02', 'firsty_lasty_02@cruddur.com', 'MOCK'),
+  ('Firsty Lasty The Third',  'firsty_lasty_03', 'firsty_lasty_03@cruddur.com', 'MOCK');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
   (
-    (SELECT uuid from public.users WHERE users.handle = 'andrewbrown' LIMIT 1),
+    (SELECT uuid from public.users WHERE users.handle = 'firsty_lasty_01' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
   )
--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -8,12 +8,14 @@ import SigninPage from './pages/SigninPage';
 import RecoverPage from './pages/RecoverPage';
 import MessageGroupsPage from './pages/MessageGroupsPage';
 import MessageGroupPage from './pages/MessageGroupPage';
+import MessageGroupNewPage from './pages/MessageGroupNewPage';
 import ConfirmationPage from './pages/ConfirmationPage';
 import React from 'react';
 import {
   createBrowserRouter,
   RouterProvider
 } from "react-router-dom";
+
 import { Amplify } from 'aws-amplify';
 
 Amplify.configure({
@@ -49,7 +51,11 @@ const router = createBrowserRouter([
     element: <MessageGroupsPage />
   },
   {
-    path: "/messages/@:handle",
+    path: "/messages/new/:handle",
+    element: <MessageGroupNewPage />
+  },
+  {
+    path: "/messages/:message_group_uuid",
     element: <MessageGroupPage />
   },
   {

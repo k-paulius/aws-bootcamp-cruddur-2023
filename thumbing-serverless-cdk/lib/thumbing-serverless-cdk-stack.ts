277d94ae6d0ab34f2a83273717b080f39e4cef5e
k-paulius <k.dash.paulius@gmail.com>
2023-05-08 00:00:00 -0500

    Updated CDK to use existing S3 bucket, instead of creating it; adding SNS topic and S3 notifications for Lambda and SNS

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,10 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
 import { Construct } from 'constructs';
 import * as dotenv from 'dotenv';
+import * as dotenvExpand from 'dotenv-expand';
 
-dotenv.config();
+dotenvExpand.expand(dotenv.config());
 
 export class ThumbingServerlessCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -15,16 +20,38 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     const functionPath: string = process.env.THUMBING_FUNCTION_PATH as string;
     const folderInput: string = process.env.THUMBING_S3_FOLDER_INPUT as string;
     const folderOutput: string = process.env.THUMBING_S3_FOLDER_OUTPUT as string;
+    const webhookUrl: string = process.env.THUMBING_WEBHOOK_URL as string;
+    const topicName: string = process.env.THUMBING_TOPIC_NAME as string;
 
-    const bucket = this.createBucket(bucketName);
+    //const bucket = this.createBucket(bucketName);
+    const bucket = this.importBucket(bucketName);
     const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
+
+    // add S3 event notification
+    this.createS3NotifyToLambda(folderInput, lambda, bucket);
+
+    // create policies
+    const s3ReadWritePolicy = this.createPolicyBucketAccess(bucket.bucketArn);
+    lambda.addToRolePolicy(s3ReadWritePolicy);
+
+    // create topic and subscription
+    const snsTopic = this.createSnsTopic(topicName);
+    this.createSnsSubscription(snsTopic, webhookUrl);
+
+    // add our s3 event notifications
+    this.createS3NotifyToSns(folderOutput, snsTopic, bucket);
   }
 
   createBucket(bucketName: string): s3.IBucket {
-    const bucket = new s3.Bucket(this, 'ThumbingBucket', {
+    const bucket = new s3.Bucket(this, 'AssetsBucket', {
       bucketName: bucketName,
       removalPolicy: cdk.RemovalPolicy.DESTROY
     });
+    return bucket;
+  }
+
+  importBucket(bucketName: string): s3.IBucket {
+    const bucket = s3.Bucket.fromBucketName(this, 'AssetsBucket', bucketName);
     return bucket;
   }
 
@@ -42,5 +69,50 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
       }
     });
     return lambdaFunction;
+  }
+
+  createS3NotifyToLambda(prefix: string, lambda: lambda.IFunction, bucket: s3.IBucket): void {
+    const destination = new s3n.LambdaDestination(lambda);
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      destination,
+      {prefix: prefix} // folder to contain the original images
+    );
+  }
+
+  createPolicyBucketAccess(bucketArn: string): iam.PolicyStatement {
+    const s3ReadWritePolicy = new iam.PolicyStatement({
+      actions: [
+        's3:GetObject',
+        's3:PutObject'
+      ],
+      resources: [
+        `${bucketArn}/*`
+      ]
+    });
+    return s3ReadWritePolicy;
+  }
+
+  createSnsTopic(topicName: string): sns.ITopic {
+    const snsTopic = new sns.Topic(this, 'ThumbingTopic', {
+      topicName: topicName
+    });
+    return snsTopic;
+  }
+
+  createSnsSubscription(snsTopic: sns.ITopic, webhookUrl: string): sns.Subscription {
+    const snsSubscription = snsTopic.addSubscription(
+      new subscriptions.UrlSubscription(webhookUrl)
+    );
+    return snsSubscription;
+  }
+
+  createS3NotifyToSns(prefix: string, snsTopic: sns.ITopic, bucket: s3.IBucket): void {
+    const destination = new s3n.SnsDestination(snsTopic);
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT, 
+      destination,
+      {prefix: prefix}
+    );
   }
 }

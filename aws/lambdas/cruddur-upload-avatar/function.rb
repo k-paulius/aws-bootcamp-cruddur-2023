b8b0b7e22655f313301575cf7fc39590abc01a03
k-paulius <k.dash.paulius@gmail.com>
2023-07-04 22:57:15 -0500

    Use JWT sub (aka Cognito user name) + extension for S3 object name; check we are only processing POST requests

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -2,16 +2,29 @@ require 'aws-sdk-s3'
 require 'json'
 
 def handler(event:, context:)
+  puts "Event:"
   puts event
-  s3 = Aws::S3::Resource.new
-  bucket_name = ENV["THUMBING_UPLOADS_BUCKET_NAME"]
-  object_key = 'mock.jpg'
 
-  obj = s3.bucket(bucket_name).object(object_key)
-  url = obj.presigned_url(:put, expires_in: 60 * 5)
-  url # this is the data that will be returned
-  body = {url: url}.to_json
-  { statusCode: 200, body: body }
+  if event["routeKey"] == "POST /avatars/key_upload"
+    body_hash = JSON.parse(event["body"])
+    extension = body_hash["extension"]
+    cognito_user_uuid = event["requestContext"]["authorizer"]["jwt"]["claims"]["sub"]
+    puts cognito_user_uuid
+
+    s3 = Aws::S3::Resource.new
+    bucket_name = ENV["THUMBING_UPLOADS_BUCKET_NAME"]
+    object_key = "#{cognito_user_uuid}.#{extension}"
+    puts({object_key: object_key}.to_json)
+
+    obj = s3.bucket(bucket_name).object(object_key)
+    url = obj.presigned_url(:put, expires_in: 60 * 5)
+    url # this is the data that will be returned
+    body = {url: url}.to_json
+    {
+      statusCode: 200,
+      body: body
+    }
+  end
 end
 
 puts handler(

1100cf5805c41916c8ef4b2885f03305fa2244a1
k-paulius <k.dash.paulius@gmail.com>
2023-07-22 15:12:20 -0500

    Move common build variables to a shared file

--- bin/cfn/config.sh
+++ bin/cfn/config.sh
@@ -3,12 +3,33 @@
 DIR_PATH=$(dirname $(readlink -f $0))
 BUILD_DIR="$DIR_PATH/../../build"
 
+# Application Info
 APP_ID="cruddur"
 APP_ENV="prod"
-# Artifact Bucket
+DB_PORT=5432
+FRONTEND_APP_PORT=3000
+BACKEND_APP_PORT=4567
+
+# Artifact Bucket Info
 CFN_STATE_BUCKET=$(aws ssm get-parameter --name "/$APP_ID/$APP_ENV/aws/cfn/state-bucket-name" --query 'Parameter.Value' --output text)
 CFN_S3_PREFIX="cfn"
 SAM_S3_PREFIX="sam"
 
+# Stack Name SSM Parameters
+NETWORKING_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/networking-stack-name"
+DNS_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/dns-stack-name"
+DB_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/db-stack-name"
+ECR_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/ecr-stack-name"
+CLUSTER_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/cluster-stack-name"
+SERVICE_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/service-stack-name"
+DDB_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/ddb-stack-name"
+
 # Stack Names
-DDB_STACK_NAME=$(aws ssm get-parameter --name "/${APP_ID}/${APP_ENV}/aws/cfn/ddb-stack-name" --query 'Parameter.Value' --output text)
+CFN_STATE_BUCKET_STACK_NAME="$APP_ID-$APP_ENV-cfn-state-bucket"
+NETWORKING_STACK_NAME=$(aws ssm get-parameter --name $NETWORKING_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)
+DNS_STACK_NAME=$(aws ssm get-parameter --name $DNS_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)
+DB_STACK_NAME=$(aws ssm get-parameter --name $DB_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)
+ECR_STACK_NAME=$(aws ssm get-parameter --name $ECR_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)
+CLUSTER_STACK_NAME=$(aws ssm get-parameter --name $CLUSTER_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)
+SERVICE_STACK_NAME=$(aws ssm get-parameter --name $SERVICE_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)
+DDB_STACK_NAME=$(aws ssm get-parameter --name $DDB_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)

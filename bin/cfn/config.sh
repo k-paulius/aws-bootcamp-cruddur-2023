3e2ea375bc6f7372df5e2f4382c66c6a9e4680a8
k-paulius <k.dash.paulius@gmail.com>
2023-07-23 12:48:46 -0500

    Adding CFN template for frontend deployment

--- bin/cfn/config.sh
+++ bin/cfn/config.sh
@@ -23,6 +23,7 @@ ECR_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/ecr-stack-name"
 CLUSTER_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/cluster-stack-name"
 SERVICE_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/service-stack-name"
 DDB_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/ddb-stack-name"
+FRONTEND_STACK_NAME_SSM_PARAM="/${APP_ID}/${APP_ENV}/aws/cfn/frontend-stack-name"
 
 # Stack Names
 CFN_STATE_BUCKET_STACK_NAME="$APP_ID-$APP_ENV-cfn-state-bucket"
@@ -33,3 +34,4 @@ ECR_STACK_NAME=$(aws ssm get-parameter --name $ECR_STACK_NAME_SSM_PARAM --query 
 CLUSTER_STACK_NAME=$(aws ssm get-parameter --name $CLUSTER_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)
 SERVICE_STACK_NAME=$(aws ssm get-parameter --name $SERVICE_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)
 DDB_STACK_NAME=$(aws ssm get-parameter --name $DDB_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)
+FRONTEND_STACK_NAME=$(aws ssm get-parameter --name $FRONTEND_STACK_NAME_SSM_PARAM --query 'Parameter.Value' --output text)

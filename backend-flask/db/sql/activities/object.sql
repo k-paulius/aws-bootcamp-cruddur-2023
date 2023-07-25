27256bc6bc0aaaabce56ce98ddba825f83c7f6b4
k-paulius <k.dash.paulius@gmail.com>
2023-07-25 09:27:52 -0500

    Implement reply functionality

--- backend-flask/db/sql/activities/object.sql
+++ backend-flask/db/sql/activities/object.sql
@@ -4,8 +4,9 @@ SELECT
   users.handle,
   activities.message,
   activities.created_at,
-  activities.expires_at
+  activities.expires_at,
+  activities.reply_to_activity_uuid
 FROM public.activities
-INNER JOIN public.users ON users.uuid = activities.user_uuid 
-WHERE 
+INNER JOIN public.users ON users.uuid = activities.user_uuid
+WHERE
   activities.uuid = %(uuid)s
--- frontend-react-js/src/components/ReplyForm.js
+++ frontend-react-js/src/components/ReplyForm.js
@@ -1,6 +1,7 @@
 import './ReplyForm.css';
 import React from "react";
 import process from 'process';
+import {getAccessToken} from '../lib/CheckAuth';
 
 import ActivityContent  from '../components/ActivityContent';
 
@@ -18,13 +19,17 @@ export default function ReplyForm(props) {
     event.preventDefault();
     try {
       const backend_url = `${process.env.REACT_APP_BACKEND_URL}/api/activities/${props.activity.uuid}/reply`
+      await getAccessToken()
+      const access_token = localStorage.getItem("access_token")
       const res = await fetch(backend_url, {
         method: "POST",
         headers: {
+          'Authorization': `Bearer ${access_token}`,
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         },
         body: JSON.stringify({
+          activity_uuid: props.activity.uuid,
           message: message
         }),
       });
@@ -37,7 +42,6 @@ export default function ReplyForm(props) {
           return element.uuid ===  props.activity.uuid;
         });
         found_activity.replies.push(data)
-
         props.setActivities(activities_deep_copy);
         // reset and close the form
         setCount(0)

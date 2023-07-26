4232af811861addf94a0d63919d5cf3c786beed7
k-paulius <k.dash.paulius@gmail.com>
2023-07-25 23:55:03 -0500

    Fix ActivityShowItem formatting issues

--- frontend-react-js/src/components/ActivityShowItem.js
+++ frontend-react-js/src/components/ActivityShowItem.js
@@ -10,6 +10,14 @@ import { format_datetime, time_ago, time_future } from '../lib/DateTimeFormats';
 import {ReactComponent as BombIcon} from './svg/bomb.svg';
 
 export default function ActivityShowItem(props) {
+  let expires_at;
+  if (props.activity.expires_at) {
+    expires_at = <div className="expires_at" title={format_datetime(props.activity.expires_at)}>
+                    <BombIcon className='icon' />
+                    <span className='ago'>{time_ago(props.activity.expires_at)}</span>
+                  </div>
+
+  }
 
   const attrs = {}
   attrs.className = 'activity_item expanded'
@@ -17,8 +25,8 @@ export default function ActivityShowItem(props) {
     <div {...attrs}>
       <div className="acitivty_main">
         <div className='activity_content_wrap'>
+          <Link className='activity_avatar'to={`/@`+props.activity.handle} ></Link>
           <div className='activity_content'>
-            <Link className='activity_avatar'to={`/@`+props.activity.handle} ></Link>
             <div className='activity_meta'>
               <div className='activity_identity' >
                 <Link className='display_name' to={`/@`+props.activity.handle}>{props.activity.display_name}</Link>
@@ -28,18 +36,15 @@ export default function ActivityShowItem(props) {
                 <div className="created_at" title={format_datetime(props.activity.created_at)}>
                   <span className='ago'>{time_ago(props.activity.created_at)}</span>
                 </div>
-                <div className="expires_at" title={format_datetime(props.activity.expires_at)}>
-                  <BombIcon className='icon' />
-                  <span className='ago'>{time_future(props.activity.expires_at)}</span>
-                </div>
+                {expires_at}
               </div>{/* activity_times */}
             </div>{/* activity_meta */}
+            <div className="message">{props.activity.message}</div>
           </div>{/* activity_content */}
-          <div className="message">{props.activity.message}</div>
         </div>
 
         <div className='expandedMeta'>
-          <div class="created_at">
+          <div className="created_at">
             {format_datetime(props.activity.created_at)}
           </div>
         </div>
